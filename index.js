0d9e46156099ea309136683443d3308329187ff2
samar-soni <112100068+samar-soni@users.noreply.github.com>
2023-10-23 22:07:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ randomNumber1=Math.round(randomNumber1);
 var randomNumber2=Math.random();
 randomNumber2=randomNumber2*5 + 1;
 randomNumber2=Math.round(randomNumber2);
-document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
-document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+document.querySelector(".img1").setAttribute("src","dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","dice"+randomNumber2+".png");
 if(randomNumber1>randomNumber2)
 {
     document.querySelector("h1").innerHTML="Player 1 is winner";
